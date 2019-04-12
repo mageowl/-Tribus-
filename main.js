a38401775bbdfd86d8f646b92e6ee98d6adbcae2
Addison <asharpseattle@gmail.com>
2019-04-12 11:59:07 -0700

    Update main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+
+//input placeholder
+var input = "yes"
+
+//Main menu
+console.log(">> Welcome to Tribus <<")
+console.log("Your job is to restore the power between\n two worlds that rely on eachother")
+console.log("By reconecting another world to them that split from them centreys ago.")
+console.log("\n\n")
